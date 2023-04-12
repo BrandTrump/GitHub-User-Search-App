175b42f7e731991d1984d9e929ab8d811a43e48c
Brandon Trump <brandontrump101@gmail.com>
2023-04-12 14:22:48 +1200

    feat: added the dark & light mode button component to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import styles from "../styles/Header.module.scss";
+import DarkModeButton from "./DarkModeButton";
 
 function Header() {
   return (
     <header className={styles.heading_container}>
       <h1>devfinder</h1>
       <div className={styles.darkmode}>
-        <h3>LIGHT</h3>
+        <DarkModeButton />
       </div>
     </header>
   );
