0c4330d372e71ae40fa4ede3752581701bf849f1
Brandon Trump <brandontrump101@gmail.com>
2023-04-12 14:24:29 +1200

    gave the h1 tag a class name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "@/app/page.module.scss";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Find any GitHub user!</h1>
+      <h1 className={styles.search_cta}>Find any GitHub user!</h1>
     </div>
   );
 }
